44ba203e8ffe0bdfd181b259548ba400341f8bd5
umangsaltuniv <umang@saltuniv.com>
2020-09-02 13:18:53 +0530

    CodeCommit1.0

---
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -0,0 +1,65 @@
+package com.expense.tests;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import net.rcarz.jiraclient.BasicCredentials;
+import net.rcarz.jiraclient.Field;
+import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.JiraClient;
+
+
+
+public class ExpenseManagerSystemTest {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void beforeTest() {
+		
+	
+		driver = new HtmlUnitDriver();
+		
+		//Thread.sleep(600000);	
+	}
+	
+	
+	@Test
+	public void titleTest1() throws Exception{
+		
+		driver.get("http://localhost:8089/ExpenseApp-1/login.jsp");	
+		
+		
+		//Buggy Code
+		String expectedTitle = "Hello Page";
+		
+		//Correct Code
+		//String expectedTitle = "Login page";
+		
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(expectedTitle, actualTitle);
+        System.out.println(actualTitle);
+	}
+	
+	@AfterMethod
+	public void afterTest(ITestResult result) throws Exception {
+		
+		driver.quit();
+		
+        if(result.getStatus() == ITestResult.FAILURE) {
+        	BasicCredentials cred = new BasicCredentials("verityamazon10", "Password@1");
+        	JiraClient jira = new JiraClient("http://100.25.133.40:8080/", cred);
+        	Issue issueName = jira.createIssue("AUT", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
+        	System.out.println("Issue is created in Jira with Issue Key: "+issueName.getKey());
+        }
+
+	}
+	
+	
+}
