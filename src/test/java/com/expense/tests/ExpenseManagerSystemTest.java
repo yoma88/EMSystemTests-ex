164c1f978e5f931ace674fd4faa08f65e7e0a039
umangsaltuniv <umang@saltuniv.com>
2021-04-14 11:47:44 +0530

    CodeCommit1.0

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -57,7 +57,7 @@ public class ExpenseManagerSystemTest {
         	BasicCredentials cred = new BasicCredentials("verityamazon33@gmail.com", "Password@1");
 			
 			//You need to enter your JIRA machine IP address with port 8080 in below line
-        	JiraClient jira = new JiraClient("http://3.233.239.83:8080/", cred);
+        	JiraClient jira = new JiraClient("http://3.233.239.83:8080/secure/Dashboard.jspa", cred);
 			
 			//You need to enter your JIRA project key in below line
         	Issue issueName = jira.createIssue("AUT", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
